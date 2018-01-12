06ed340eb3d8163577c25c2713368b163e8d001f
rarhoade <rarhoade@umich.edu>
2018-01-12 15:12:48 -0500

    added bullet functionality

---
+++ Assets/Scripts/PlayerWeapon.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerWeapon : MonoBehaviour {
+
+    PlayerDirection playerDirection;
+
+    public GameObject bulletPrefab;
+
+    public Transform firingPositionForward;
+    public Transform firingPositionUpward;
+
+    public float firingSpeed = 10f;
+
+	// Use this for initialization
+	void Awake () {
+        playerDirection = GetComponentInParent<PlayerDirection>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetKeyDown(KeyCode.X))
+        {
+            GameObject bulletInstance = GameObject.Instantiate(bulletPrefab);
+
+            if (playerDirection.IsLookingUp())
+            {
+                bulletInstance.transform.position = firingPositionUpward.position;
+                bulletInstance.GetComponent<Rigidbody>().velocity = Vector3.up * firingSpeed;
+            }
+            else
+            {
+                bulletInstance.transform.position = firingPositionForward.position;
+                if (playerDirection.IsFacingRight())
+                {
+                    bulletInstance.GetComponent<Rigidbody>().velocity = Vector3.right * firingSpeed;
+                }
+                else
+                {
+                    bulletInstance.GetComponent<Rigidbody>().velocity = Vector3.left * firingSpeed;
+                }
+            }
+        }
+	}
+}
