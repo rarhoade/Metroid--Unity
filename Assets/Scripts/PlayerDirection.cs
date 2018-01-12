67873289a3218c5a55c090f754deac51f9a380a1
rarhoade <rarhoade@umich.edu>
2018-01-12 14:21:26 -0500

    got up to firing bullets in the tutorial

---
+++ Assets/Scripts/PlayerDirection.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerDirection : MonoBehaviour {
+
+    public SpriteRenderer spriteRenderer;
+
+    public Sprite spriteLookingForward;
+    public Sprite spriteLookingUpward;
+
+    bool lookingUp = false;
+    bool facingRight = true;
+
+	
+	// Update is called once per frame
+	void Update () {
+        // Horizontal
+        float horizontalAxis = Input.GetAxis("Horizontal");
+        if(facingRight && horizontalAxis < 0)
+        {
+            facingRight = false;
+            this.transform.localScale = new Vector3(-1, 1, 1);
+        }
+        else if (!facingRight && horizontalAxis > 0)
+        {
+            facingRight = true;
+            this.transform.localScale = new Vector3(1, 1, 1);
+        }
+
+        // Vertical
+        bool holdingUp = Input.GetKey(KeyCode.UpArrow);
+        if(lookingUp && !holdingUp)
+        {
+            lookingUp = false;
+            spriteRenderer.sprite = spriteLookingForward;
+        }
+        else if(!lookingUp && holdingUp)
+        {
+            lookingUp = true;
+            spriteRenderer.sprite = spriteLookingUpward;
+        }
+	}
+
+    public bool IsFacingRight()
+    {
+        return facingRight;
+    }
+
+    public bool IsLookingUp()
+    {
+        return lookingUp;
+    }
+}
