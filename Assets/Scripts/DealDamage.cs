634f3e777d7a6290b4eeb85b0521d8f9cbf5c731
EtherealAurora <zaaron@umich.edu>
2018-01-14 16:47:19 -0500

    Bullet damage and knockback framework

--- Assets/Scripts/DealDamage.cs
+++ Assets/Scripts/DealDamage.cs
@@ -4,13 +4,14 @@ using UnityEngine;
 
 public class DealDamage : MonoBehaviour {
     public int myDamage = 8;
+    public float knockback = 10f;
 
     private void OnCollisionEnter(Collision collision)
     {
         UnitHealth uh = collision.gameObject.GetComponent<UnitHealth>();
         if (uh != null)
         {
-            uh.TakeDamage(myDamage);
+            uh.TakeDamage(myDamage, knockback);
         }
     }
 }
