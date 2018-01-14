f94d961aa468cca2ec03fd34ecabb29fbc27f963
EtherealAurora <zaaron@umich.edu>
2018-01-14 01:29:24 -0500

    adding DealDamage and UnitHealth scripts

---
+++ Assets/Scripts/DealDamage.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DealDamage : MonoBehaviour {
+    public int myDamage = 8;
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        UnitHealth uh = collision.gameObject.GetComponent<UnitHealth>();
+        if (uh != null)
+        {
+            uh.TakeDamage(myDamage);
+        }
+    }
+}
---
+++ Assets/Scripts/UnitHealth.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UnitHealth : MonoBehaviour {
+
+    public int healthTotal = 30;
+    public bool invulnerability = false;
+    public int numberOfBlinks = 2;
+    public float blink = 0.4f;
+
+    // Update is called once per frame
+    void Update () {
+		if(Input.GetKeyDown(KeyCode.Alpha1))
+        {
+            invulnerability = !invulnerability;
+        }
+	}
+
+    public void TakeDamage(int damage)
+    {
+        if (!invulnerability)
+        {
+            healthTotal -= damage;
+            StartCoroutine(IFrames());
+        }
+        if (healthTotal <= 0)
+        {
+            //TODO Die
+        }
+    }
+
+    IEnumerator IFrames()
+    {
+        SpriteRenderer[] sprites = gameObject.GetComponentsInChildren<SpriteRenderer>();
+        for (int i = 0; i < numberOfBlinks; i++)
+        {
+            SetSpritesAlpha(sprites, 0.5f);
+            yield return new WaitForSeconds(blink);
+            SetSpritesAlpha(sprites, 1f);
+            yield return new WaitForSeconds(blink);
+        }
+    }
+
+    private void SetSpritesAlpha(SpriteRenderer[] s, float alpha)
+    {
+        for (int i = 0; i < s.Length; i++)
+        {
+            Color c = s[i].color;
+            s[i].color = new Color(c.r, c.g, c.b, alpha);
+        }
+    }
+}
