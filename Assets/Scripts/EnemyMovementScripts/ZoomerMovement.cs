cc0f46493f909454257b6346571a7e5d572020aa
rarhoade <rarhoade@umich.edu>
2018-01-24 01:05:00 -0500

    added basic layout of the map

--- Assets/Scripts/EnemyMovementScripts/ZoomerMovement.cs
+++ Assets/Scripts/EnemyMovementScripts/ZoomerMovement.cs
@@ -3,12 +3,60 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class ZoomerMovement : MonoBehaviour {
-	//Face to Direction Movement Mapping
-	//Left: Up (y+)
-	//Right: Down (y-)
-	//Up: Right (x+)
-	//Down: Left (x-)
-	public enum Direction{Left,Right,Up,Down}; //based on what direction the spikes are facing
+
+    Rigidbody rigid;
+    SpriteRenderer spriteRend;
+    public Vector3 startVel = Vector3.right;
+    public float zoomerSpeed = 1.0f;
+
+    void Start()
+    {
+        rigid = GetComponent<Rigidbody>();
+        //zoomPos = GetComponent<Transform>();
+        spriteRend = GetComponent<SpriteRenderer>();
+        rigid.velocity = startVel * zoomerSpeed;
+    }
+
+    private void FixedUpdate()
+    {
+        rayCastCheck();
+        changeWallCheck();
+        //rigid.velocity = startVel * zoomerSpeed;
+    }
+
+    void rayCastCheck()
+    {
+        Debug.DrawRay(transform.position, -transform.up * 0.65f, Color.white);
+        if (!Physics.Raycast(transform.position, -transform.up, 0.65f))
+        {
+            transform.position = transform.position + (transform.right - transform.up) * 0.6f;
+            transform.Rotate(Vector3.forward * -90);
+            rigid.velocity = transform.right * zoomerSpeed;
+            //Debug.Log("Velocity:" + rigid.velocity);
+            Debug.DrawRay(transform.position, -transform.up * 0.65f, Color.blue);
+        }
+    }
+
+    private void changeWallCheck()
+    {
+        RaycastHit hit;
+        bool wallCheck = Physics.Raycast(transform.position, transform.right, out hit, 0.6f);
+        //Debug.DrawRay(transform.position, transform.right * 0.55f, Color.red);
+        //Debug.Log(hit.collider.gameObject.name);
+        if (wallCheck && hit.collider.gameObject.layer==0)//(hit.collider.gameObject.name == "Tile_WALL" || hit.collider.gameObject.name == "Tile_NONE" || hit.collider.gameObject.name == "Tile_DOOR" || hit.collider.gameObject.name == "Tile_LAVA 1"))
+        {
+            //Debug.Log("Hit a wall");
+            transform.Rotate(Vector3.forward * 90);
+            rigid.velocity = transform.right * zoomerSpeed;
+        }
+    }
+
+    //Face to Direction Movement Mapping
+    //Left: Up (y+)
+    //Right: Down (y-)
+    //Up: Right (x+)
+    //Down: Left (x-)
+    /*public enum Direction{Left,Right,Up,Down}; //based on what direction the spikes are facing
 	public SpriteRenderer spriteRend;
 	public Direction currentFace;
 	public float zoomerSpeed = 5.0f;
